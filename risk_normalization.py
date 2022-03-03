d455bfa4c975fdd245248b9c900877979b636d35
howardbandy <howardbandy@gmail.com>
2022-03-03 09:31:28 -0800

    change risk_normalization.py

--- risk_normalization.py
+++ risk_normalization.py
@@ -1,12 +1,57 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#  CAR25onCSV.py
+#
+#  A program to read a .CSV file that contains a list of trades
+#    and computes the safe-f and CAR25 metrics that best
+#    estimate the future performance of the system that produced
+#    these gains and losses.
+
+#  The risk_normalization library is managed by PyPi.
+#  Before running this program, use pip to install risk_normalization
+#  If you have set up a virtual environment, change directory to
+#    that environment.
+#  Then:
+#    pip3 install risk-normalization
+
+
+import math
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+import random
+#import risk_normalization
+import sklearn as skl
+import statistics
+import statsmodels as st
+
+#  These do not have a __version__ method
+#print (f'math version:               {math.__version__}')
+#print (f'random version:             {random.__version__}')
+#print (f'risk_normalization version: {risk_normalization.__version__}')
+#print (f'statistics version:         {statistics.__version__}')
+
+#  These do
+#print (f'matplotlib version:         {plt.__version__}')
+#print (f'numpy version:              {np.__version__}')
+#print (f'pandas version:             {pd.__version__}')
+#print (f'scikit-learn version:       {skl.__version__}')
+#print (f'statsmodels version:        {st.__version__}')
+
+#-----------------------------------------------------
 """
 risk_normalization.py
+
+Created on Mon Feb 28 19:45:18 2022
+
+@author: howard bandy
 
 This file created on Tuesday, October 9, 2020
 Modified on Friday, December 11, 2020
 ... separated risk_normalization and related procedures
 ... in preparation for publishing on PyPI
+Modified on Monday, February 28, 2022
+... added missing global value
+... modified calling sequence
+
 
 Risk normalization routines designed by Dr. Howard Bandy, 
 Blue Owl Press.
@@ -22,7 +67,11 @@ in 2014, as a Python program.
 #-----------------
 Overview of the function risk_normalization:
 
-The risk normalization consits of two phases:
+Begin with a set of trades.  These are analyzed as is to compute
+safe-f, and are assumed to be the best estimate of future 
+performance.  This set does not change throughout the procedure.
+
+The risk normalization consists of two phases:
 1.  Compute the maximum fraction of the trading account
     that can be used to take a position in the tradable issue
     without exceeding the personal risk tolerance of the
@@ -31,9 +80,7 @@ The risk normalization consits of two phases:
     potential for the forecast period.  Convert the gain
     to Compound Annual rate of Return, called "CAR25"
 
-Begin with a set of trades.  These are analyzed as is to compute
-safe-f, and are assumed to be the best estimate of future 
-performance.  This set does not change throughout the procedure.
+#--- Compute safe-f
 
 Set the fraction an initial value of 1.00
     Create many equally likely equity curves,
@@ -51,6 +98,8 @@ Set the fraction an initial value of 1.00
 
 safe-f has been established.
 
+#--- Compute CAR25
+
 Using safe-f as the fraction
     Create many equally likely equity curves,
         measure the final equity,
@@ -61,7 +110,8 @@ Using safe-f as the fraction
         compound annual rate of return.
         That value is car25.
 
-Return safe-f and CAR25
+Return the mean and standard deviation of both safe-f and CAR25
+
 #-----------------
 
 Assumptions
@@ -87,37 +137,47 @@ Use:
                     initial_capital,
                     tail_percentile,
                     drawdown_tolerance,
-                    number_equity_in_CDF
+                    number_equity_in_CDF,
+                    number_repetitions
                   )
 
 Parameters:
   trades:  The set of trades to evaluate.
       Expecting a numpy array with one dimension.
   number_days_in_forecast:  the forecast period.
-      504 for a forecast of 2 years.
+      Typical = 504 for a 2 year forecast
+      Typical = 252 for a 1 year forecast
   number_trades_in_forecast:
       The number of trades to draw for each equity sequence.
-        Same as number_days_in_forecast if
-          marking to market and trading daily.
-        A smaller number if trade data
-          represents multiday holding.
+      If each trade represents one daya of market-to-market result:
+          If there is an entry for each and every day of the 
+              trading, including entries of 0.0 for days not traded: 
+              Typical = the same as number_days_in_forecast.
+          If there are entries for only those days traded:
+              Typical = the expected number of days with trades
+                        in the forecast period.
+      If each trade represents a multiday trade:
+          Typical = the expected number of trades in the forecast period.
   initial_capital:  initial amount in the trading account.
       Typical = $100,000.00
-  tail_percentile:  The percentile at which to measure the
-      tail risk.  Typical = 5  => the 95th percentile
-      of the CDF.
+  tail_percentile:  The percentile at which to measure the tail risk.  
+      Typical = 5  => the 95th percentile of the CDF.
   drawdown_tolerance:  The traders drawdown tolerance.
       Expressed as a proportion of maximum equity to date.
       Typical = 0.10  A 10% drawdown.
-  number_equity_in_CDF:  The number of equity curves used
-      to compute a single CDF.  Typical = 1000
+  number_equity_in_CDF:  The number of equity curves used 
+      to compute a single CDF.  
+      Typical = 1000
+  number_repetitions:  The number of replications of calculation of 
+      safe-f and CAR25 to compute the mean and standard deviation.  
+      Typical = 10
 
 Returns:
-  safe_f:  The fraction of the trading account that will be
-      used for each trade.
+  safe_f_mean:  The fraction of the trading account that will be
+                used for each trade.
   safe_f_stdev:  standard deviation of safe_f calculations.     
-  CAR25:  The compound annual rate of return for the given
-      set of trades and position size.
+  CAR25_mean:  The compound annual rate of return for the given
+                 set of trades and position size.
   CAR25_stdev:  standard deviation of CAR25 calculations.     
 
 
@@ -127,17 +187,12 @@ drawdown:                 list used to accumulate day by day drawdown
 max_drawdown              maximum drawdown to date
 equity:                   list used to accumulate day by day equity
 max_equity:               maximum equity to date
-
 file_name:                name of csv or txt file containing trades
 fraction:                 during calculations, the then current estimate
                               of position size, safe-f
 
-number_repetitions:       number of replications of calculation of 
-                              safe-f and CAR25 to compute the 
-                              mean and standard deviation.  Typically 10.
-number_trades_in_best_est: number of trades in best estimate set 
-                                  of trades
 """
+
 
 def make_one_equity_sequence(
     trades,
@@ -154,8 +209,8 @@ def make_one_equity_sequence(
     trades:           the set of trades to be analyzed
     fraction:         the proportion of the trading account
                       to be used for each trade.
-    number_days_in_forecast:    Length in days of forecast.                  
-    number_trades_in_forecast:  Length in trades of the equity sequence.
+    number_days_in_forecast:    Length of forecast in days.                 
+    number_trades_in_forecast:  Length of forecast in trades.
     initial_capital:  Starting value of the trading account.
     
     Returns:  
@@ -190,11 +245,12 @@ def make_one_equity_sequence(
     #  if necessary, fill remaining days    
     for i in range(number_trades_in_forecast,number_days_in_forecast):
         daily_equity[i] = equity
-        
+   
 #    plt.plot(daily_equity)
 #    plt.show()
-    
+
     return (equity, max_drawdown)
+
 
 def analyze_distribution_of_drawdown(
     trades,
@@ -227,10 +283,10 @@ def analyze_distribution_of_drawdown(
     sorted_max_dd = np.sort(max_dd_list)
 #    plt.plot(sorted_max_dd)
 #    plt.show()
-
     tail_risk = np.percentile(sorted_max_dd, 100 - tail_percentile)
 
     return tail_risk
+
 
 def form_distribution_of_equity(
     trades,
@@ -261,10 +317,11 @@ def form_distribution_of_equity(
 
     return sorted_equity
 
+
 def risk_normalization(
         trades, 
         number_days_in_forecast, 
-        number_of_trades_in_forecast,
+        number_trades_in_forecast,
         initial_capital, 
         tail_percentile, 
         drawdown_tolerance, 
@@ -275,6 +332,8 @@ def risk_normalization(
     safe_fs = []
     TWR25s = []
     CAR25s = []
+    
+    desired_accuracy = 0.003
     
     for rep in range(number_repetitions):
     
@@ -287,13 +346,13 @@ def risk_normalization(
         while not done:
             # print(f"fraction this pass:  {fraction:0.3f}")
             tail_risk = analyze_distribution_of_drawdown(
-                                                 trades, 
-                                                 fraction,
-                                                 number_days_in_forecast,
-                                                 number_trades_in_forecast,
-                                                 initial_capital,
-                                                 tail_percentile,
-                                                 number_equity_in_CDF)
+                            trades, 
+                            fraction,
+                            number_days_in_forecast,
+                            number_trades_in_forecast,
+                            initial_capital,
+                            tail_percentile,
+                            number_equity_in_CDF)
         
             # print(f"tail_risk this pass: {tail_risk:0.3f}")
             if abs(tail_risk - drawdown_tolerance) < desired_accuracy:
@@ -309,19 +368,21 @@ def risk_normalization(
         #  TWR25 is 25th percentile
         #  CAR25 is 25th percentile
         
-        CDF_equity = form_distribution_of_equity(trades, 
-                                                 fraction,
-                                                 number_days_in_forecast,
-                                                 number_trades_in_forecast,
-                                                 initial_capital,
-                                                 number_equity_in_CDF)
+        CDF_equity = form_distribution_of_equity(
+                         trades, 
+                         fraction,
+                         number_days_in_forecast,
+                         number_trades_in_forecast,
+                         initial_capital,
+                         number_equity_in_CDF)
+        
         TWR25 = np.percentile(CDF_equity, 25)
         # print(f'terminal wealth: {TWR25:9.0f}')
         
         CAR25 = 100.0 * (math.exp((252.0 / number_days_in_forecast) * 
                                  math.log(TWR25/initial_capital)) - 1.0)
         
-        # print(f'Compound Annual Return: {CAR25:0.3f}%')
+        print(f'Compound Annual Return: {CAR25:0.3f}%')
     
         safe_fs.append(fraction)
         TWR25s.append(TWR25)
@@ -329,7 +390,6 @@ def risk_normalization(
     
     #  end of rep loop
        
-        
     # print(safe_fs)
     # print(TWR25s)
     # print(CAR25s)
@@ -338,7 +398,7 @@ def risk_normalization(
     #        ' calculations')    
     safe_f_mean = statistics.mean(safe_fs)
     # print (f'safe_f_mean:   {safe_f_mean:0.3f}')
-    if number_repetitions > 1:
+    if number_repetitions > 2:
         safe_f_stdev = statistics.stdev(safe_fs)
     #     print (f'safe_f_stdev:  {safe_f_stdev:0.3f}')
     else:
@@ -347,7 +407,7 @@ def risk_normalization(
     
     TWR25_mean = statistics.mean(TWR25s)
     # print (f'TWR25_mean:   {TWR25_mean:0.0f}')
-    if number_repetitions > 1:
+    if number_repetitions > 2:
         TWR25_stdev = statistics.stdev(TWR25s)
     #     print (f'TWR25_stdev:  {TWR25_stdev:0.3f}')
     else:
@@ -356,7 +416,7 @@ def risk_normalization(
     
     CAR25_mean = statistics.mean(CAR25s)
     # print (f'CAR25_mean:   {CAR25_mean:0.3f}%')
-    if number_repetitions > 1:
+    if number_repetitions > 2:
         CAR25_stdev = statistics.stdev(CAR25s)
     #     print (f'CAR25_stdev:  {CAR25_stdev:0.3f}%')
     else:
@@ -364,5 +424,56 @@ def risk_normalization(
     #     print ('standard deviation calculation is not meaningful')
     
     return (safe_f_mean, safe_f_stdev, CAR25_mean, CAR25_stdev)
+
+#-----------------------------------------------------
+
+newline = '\n'
+
+#  Estimate safe-f and CAR25 for a given csv file
+
+file_name = 'RSIDailyGains.csv'
+
+#  This file covers about 28 years of daily trading.
+#  It contains 1185 trades.
+#  A 2 year forecast will have about 42 trades. 
+
+#base_path_to_trades = "/home/howard/ML4TProjects/CAR25onCSV/data/"
+base_path_to_trades = "./data/"
+path_to_trades = base_path_to_trades + file_name 
+print (f'{newline}The data file being processed is: {path_to_trades}')
+
+trades = np.loadtxt(path_to_trades)
+print (f'There are {len(trades)} marked-to-market daily trades in the file')
+print ('Here are the first 10 trades:')
+print (trades[0:10])
+
+number_days_in_forecast = 504 # 2 years
+number_trades_in_forecast = 84 # 2 * (1185 / 28)
+initial_capital = 100000.0
+tail_percentile = 5
+drawdown_tolerance = 0.10
+number_equity_in_CDF = 100
+number_repetitions = 5
+
+safe_f_mean,safe_f_stdev,CAR25_mean,CAR25_stdev = risk_normalization(
+    trades,
+    number_days_in_forecast,
+    number_trades_in_forecast,
+    initial_capital,
+    tail_percentile,
+    drawdown_tolerance,
+    number_equity_in_CDF,
+    number_repetitions)
+
+
+print (f'CAR25 mean:   {CAR25_mean:.2f}%')
+print (f'CAR25 stdev:  {CAR25_stdev:.2f}')
+print (f'safe-f mean:  {safe_f_mean:.2f}')
+print (f'safe-f stdev: {safe_f_stdev:.2f}')
+
+
+
+
+
 
 
